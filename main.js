cfca557778f59a0466782546f4f4f045d2ad1930
Camobeast <jakub.lilley@gmail.com>
2024-01-04 22:22:34 +0100

    Changed the banner image and added image carousel

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ const btn = document.querySelector('.cta-button');
 btn.addEventListener('click', () => {
   document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
 });
+
+let currentSlide = 0;
+showSlide(currentSlide);
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-images")[0].getElementsByTagName("img");
+    if (n >= slides.length) { currentSlide = 0; }
+    if (n < 0) { currentSlide = slides.length - 1; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[currentSlide].style.display = "block";
+}
+
+function moveSlide(n) {
+    showSlide(currentSlide += n);
+}
